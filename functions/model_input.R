91cde593054f979457ed18134dfd2b925847289e
Alex Solano <alexsolano@Alexs-MacBook-Air-5.local>
2025-01-17 15:58:56 -0500

    started model_input.R

--- functions/model_input.R
+++ functions/model_input.R
@@ -1,16 +1,16 @@
-#' Reads a model input file given species, month, approach and path
-#' 
-#' @param scientificname chr, the species name
-#' @param mon chr month abbreviation
-#' @param approach chr, one of "greedy" or "conservative"
-#' @param path chr the path to the data directory
-#' 
-#' @return 
-
 read_model_input = function(scientificname = "Mola mola",
                             mon = "Jan",
                             approach = "greedy",
                             path = data_path("model_input")){
+  #' Reads a model input file given species, month, approach and path
+  #' 
+  #' @param scientificname chr, the species name
+  #' @param mon chr month abbreviation
+  #' @param approach chr, one of "greedy" or "conservative"
+  #' @param path chr the path to the data directory
+  #' 
+  #' @return 
+  
   # your part goes in here
   filename = sprintf("%s-%s-%s_input.gpkg", 
                      gsub(" ", "_", scientificname),
@@ -20,5 +20,5 @@ read_model_input = function(scientificname = "Mola mola",
   
   x = read_sf(files[1])
   
-  return(x['filename'])
+  return(x[filename])
 }
