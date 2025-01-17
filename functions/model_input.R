5b9511499d52b90c32b3d9dadbbfc1bd30e3f884
Alex Solano <alexsolano@Alexs-MacBook-Air-5.local>
2025-01-17 15:56:57 -0500

    changed gitignore

---
+++ functions/model_input.R
@@ -0,0 +1,24 @@
+#' Reads a model input file given species, month, approach and path
+#' 
+#' @param scientificname chr, the species name
+#' @param mon chr month abbreviation
+#' @param approach chr, one of "greedy" or "conservative"
+#' @param path chr the path to the data directory
+#' 
+#' @return 
+
+read_model_input = function(scientificname = "Mola mola",
+                            mon = "Jan",
+                            approach = "greedy",
+                            path = data_path("model_input")){
+  # your part goes in here
+  filename = sprintf("%s-%s-%s_input.gpkg", 
+                     gsub(" ", "_", scientificname),
+                     mon)
+  
+  files = list.files(path, full.names = TRUE)
+  
+  x = read_sf(files[1])
+  
+  return(x['filename'])
+}
